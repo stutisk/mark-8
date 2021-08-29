14b919b25031d5118177ffdb85b83bd527919613
stutisk <48529714+stutisk@users.noreply.github.com>
2021-08-29 19:00:55 +0530

    update

--- src/App.js
+++ src/App.js
@@ -7,24 +7,51 @@ const emojiDictionary = {
   "🧼🖐⏲": " Wash your hands regularly ",
   "🚇😷🛒": "Wearing a mask in public"
 };
+
+var emojis = Object.keys(emojiDictionary);
 export default function App() {
   const [meaning, setMeaning] = useState("");
+
   function emojiInputHandler(event) {
     var userInput = event.target.value;
     var meaning = emojiDictionary[userInput];
+
     if (meaning === undefined) {
       meaning = "sorry we dont have the meaning of this emoji in the database";
     }
+    // showing on the view
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
     setMeaning(meaning);
   }
   return (
     <div className="App">
       <h2>
-        In order to minimize the spread of disease, suggested behaviors include
+        <em>
+          {" "}
+          "In order to minimize the spread of disease, suggested behaviors
+          include :"{" "}
+        </em>
       </h2>
 
-      <input onChange={emojiInputHandler} />
-      <h3>{meaning}</h3>
+      <input placeholder="enter emoji here" onChange={emojiInputHandler} />
+
+      <h2> {meaning} </h2>
+      <h3> emojis we know </h3>
+      {emojis.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "larger", padding: "0.2rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}{" "}
+          </span>
+        );
+      })}
     </div>
   );
 }
